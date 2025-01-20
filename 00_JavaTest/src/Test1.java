c121d38a86637a14b223b4c7dc07e9f723a5048e
kohyunwoo00 <hyunwoo36003@naver.com>
2025-01-20 11:25:33 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("이클립스 실행 중");
+	System.out.println("자동완성");
+	System.out.println("까먹음");
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,10 @@
+public class JavaTest{
+	public static void main(String[] args){
+	  System.out.println("Hello world!!!");
+           	  System.out.println("하이");
+
+	 for(int i=0; i<=5; i++){
+	  System.out.println(i  + "번째 줄 출력");
+	}
+  }
+}
