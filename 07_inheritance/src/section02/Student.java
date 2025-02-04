502c38ce6714be2b91e154827a2ad028e646f891
kohyunwoo00 <hyunwoo36003@naver.com>
2025-02-04 16:46:07 +0900

    20250204 다형성기초

--- 07_inheritance/src/section02/Student.java
+++ 07_inheritance/src/section02/Student.java
@@ -44,13 +44,39 @@ public class Student extends Person{
 		this.schoolName = schoolName;
 	}
 	
+	/** super 참조 변수(부모 참조 변수)
+	 * - 자식 내 부모 객체를 참조하는 변수
+	 * - 부모의 필드/메서드에 접근하기 위해 사용
+	 * 
+	 */
+	
+	// 연속으로 재정의
+	// Object.toString() -> Person.toString() -> Student.toString()
+	@Override // 
 	public String toString() {
-		return String.format("name : %s / age : %d / schoolName : %s", getName(), getAge(), schoolName);
+		//super.toString() : 부모의 toString() 메서드 호출
+		// -> name : 홍길동 / age : 20
+		return super.toString() + "/ schoolName : " + schoolName;
+//		return String.format("name : %s / age : %d / schoolName : %s", getName(), getAge(), schoolName);
 	}
 	
+	/* Person.introduce() 메서드 오버라이딩(재정의) */
 	
+	/* 오버라이딩 성립 조건
+	 * [변경 불가]
+	 * - 반환형
+	 * - 메서드명
+	 * - 매개변수(개수, 순서, 타입)
+	 * 
+	 * [변경 가능]
+	 * - 접근 제어자(기존 -> 넓은 범위)
+	 * - 예외 처리(기존-> 좁은 범위)
+	 */
 	
-	
+	@Override // 오버라이딩 명시 + 성립 조건 맞는지 검사 지시
+	public String introduce(String alias) {
+		return String.format("[Student가 재정의] 내 이름은 %s, %s 이죠", getName(), alias);
+	}
 	
 	
 }
---
+++ 08_polymorphism/src/section01/run/PolymorphismRun.java
@@ -0,0 +1,15 @@
+package section01.run;
+
+import section01.service.PolymorphismService;
+
+public class PolymorphismRun {
+	public static void main(String[] args) {
+		
+		PolymorphismService service = new PolymorphismService();
+		
+		service.test4(); // Child, Parent, Object 순서로 출력
+		
+		// 실행 : ctrl + f11
+		// 디버그 : f11
+	}
+}
